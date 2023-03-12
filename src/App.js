ef5e8df1d5b298dd877062753e24a58f3b35f85c
Fasih <fasihislam1999@gmail.com>
2023-03-12 19:05:17 -0300

    intial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import ErrorPage from "./Pages/ErrorPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={<Homepage />} />
+
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
